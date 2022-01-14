be11e49434adefede3e31c81cf790c9be28e7bf8
swapnil <engswapnil@gmail.com>
2022-01-14 11:46:06 +0000

    Both Smoke and Reg

--- src/test/java/stepDefinition/TestRunner.java
+++ src/test/java/stepDefinition/TestRunner.java
@@ -11,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 		stepNotifications=true,
 		glue="stepDefinition",
 		monochrome = true,
-		tags="@RegTest",
+		tags="@SmokeTest, @RegTest",
 		plugin= {"pretty","html:target/HtmlReports/result.html"}
 		)
 
